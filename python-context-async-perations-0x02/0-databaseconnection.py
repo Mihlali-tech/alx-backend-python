ddd1f2288ce0c51df0e8c9157e2e69893cf3adcb
Mihlali-tech <mihlalincayo84@gmail.com>
2025-07-09 15:05:39 +0200

    Add context manager and async operations tasks

---
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+class DatabaseConnection:
+    def __enter__(self):
+        self.conn = sqlite3.connect('users.db')
+        self.cursor = self.conn.cursor()
+        return self.cursor
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.conn.close()
+
+if __name__ == "__main__":
+    with DatabaseConnection() as cursor:
+        cursor.execute("SELECT * FROM users")
+        results = cursor.fetchall()
+        print(results)
